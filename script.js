2ddc681dd91b3c765b10c19fbdda09a69ee8abbf
kavalibhavani <147436607+kavalibhavani@users.noreply.github.com>
2023-11-16 19:46:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+window.alert("welcome");
+console.log("hello world");
+
+var firstNumber= 21;   //variables
+var secondNumber= 32;  
+
+age_of_a_person=18;
+let name="bhavani"
+
+
+result = firstNumber+secondNumber;
+
+console.log(result);
+console.log(age_of_a_person);
+console.log(name);
+
+
+//let
+
+let num1= 10;
+let num2= 5;
+
+/*
+multiline comment
+*/
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1%num2);
+console.log(num1**num2);
+console.log(num1/num2);
+
+//constant
+
+const num3=25;
+num3=8;
+console.log(num3);
